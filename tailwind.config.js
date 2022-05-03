cdf32e18de503125db7dc72d16cdf0de240a7172
João Gabriel <main.joaogabrielsg@gmail.com>
2022-05-02 22:37:51 -0300

    feat: add project of the day 01

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257E5",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
