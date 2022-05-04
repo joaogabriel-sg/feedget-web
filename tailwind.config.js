22fe61793a20180c45488f1628b8b4ed0556d8ab
João Gabriel <main.joaogabrielsg@gmail.com>
2022-05-03 23:31:50 -0300

    feat: add project of the day 02

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: "#996DFF",
           500: "#8257E5",
         },
       },
+      borderRadius: {
+        md: "4px",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
