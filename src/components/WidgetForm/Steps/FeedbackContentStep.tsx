b1f818151c602da43e48de261aa146fe7dafafc8
João Gabriel <main.joaogabrielsg@gmail.com>
2022-05-06 22:50:50 -0300

    feat: add project of the day 05

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -5,6 +5,9 @@ import { FeedbackType, feedbackTypes } from "..";
 
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
+import { Loading } from "../../Loading";
+
+import { api } from "../../../lib/api";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -19,14 +22,22 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
 
-    console.log({ screenshot, comment });
+    setIsSendingFeedback(true);
 
+    await api.post("/feedback", {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -69,10 +80,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.trim().length === 0}
+            disabled={comment.trim().length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : "Enviar feedback"}
           </button>
         </footer>
       </form>
